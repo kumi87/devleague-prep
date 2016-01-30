0d02cd855cfe731808bb7f1b00fd7b8a274465a5
kumi87 <kumiko.hewitt@gmail.com>
2016-01-29 20:02:53 -1000

    updated

---
+++ myFirstWebApp/public/events/ex2/js/index.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    // add an event listener to the `child` class.
+    // inspect the .css file
+    // when a `child` class is clicked your code will add CSS styles which will show the
+    // secret toy element. Click on an element will only show that element's toy, no one elses.
+
+    var childClassElements;
+    var i;
+    var childClassElement;
+
+    function setUpChildClassClickHandler(childClassElem) {
+        function evenHandler() {
+            var paragraphElement = childClassElem.children[1]; //[1]means second child (stared from 0)
+            if (paragraphElement.classList.contains('show')) {
+                paragraphElement.classList.remove('show');
+            } else {
+                paragraphElement.classList.add('show');
+            }
+        }
+        childClassElem.addEventListener('click', evenHandler);
+    }
+
+    childClassElements = document.getElementsByClassName('child');
+    for (i = 0; i < childClassElements.length; i++) {
+        childClassElement = childClassElements[i];
+        setUpChildClassClickHandler(childClassElement);
+    }
+}
